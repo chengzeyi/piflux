390368161c6e265854d84e86a1466f11474e5d45
chengzeyi <ichengzeyi@gmail.com>
2024-09-24 12:44:25 +0800

    fix build

--- src/piflux/__init__.py
+++ src/piflux/__init__.py
@@ -4,6 +4,12 @@ import torch.distributed as dist
 
 from . import adapters, config, ops  # noqa: F401
 
+try:
+    from ._version import version as __version__, version_tuple
+except ImportError:
+    __version__ = "unknown version"
+    version_tuple = (0, 0, "unknown version")
+
 
 def setup(**kwargs) -> None:
     dist.init_process_group(backend=config.dist.backend, world_size=config.dist.world_size, **kwargs)
